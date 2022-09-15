43a1acab3d6cedc8541b1f3355154c5959bd61f8
linlong <developerlin123@gmail.com>
2022-09-15 18:58:04 +0200

    create recipe model and basic shopping list,shoing edit layout

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients : Ingredient[] = [
+    new Ingredient ("apples", 10),
+    new Ingredient ("tomatos", 10),
+    new Ingredient ("cheese", 10)
+  ]
 
   constructor() { }
 
