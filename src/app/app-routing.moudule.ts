961b828d28749e70ee31751b013a2e2282212f70
linlong <developerlin123@gmail.com>
2023-01-05 13:03:49 +0100

    set up header nav, fixed page reload, set up recipes children route

--- src/app/app-routing.moudule.ts
+++ src/app/app-routing.moudule.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 const appRoutes:Routes=[
@@ -9,7 +11,13 @@ const appRoutes:Routes=[
         pathMatch:'full'
     },
     {
-        path:'recipes',component:RecipesComponent
+        path:'recipes',
+        component:
+        RecipesComponent,
+        children:[
+            {path:'',component:RecipeStartComponent},
+            {path:'id',component:RecipeDetailComponent}
+        ]
     },
     {
         path:'shopping-list',component:ShoppingListComponent
