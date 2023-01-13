f19805677a65f1576c3485e5af6e2f4dee92645d
linlong <developerlin123@gmail.com>
2023-01-13 18:40:58 +0100

    implement add more ingredients at edit page

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -60,4 +60,13 @@ export class RecipeEditComponent implements OnInit {
     })
   }
 
+  onAddIngredient(){
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    )
+  }
+
 }
