e1ce131ad95635aa3918ce4a7f09a012815c2a1b
linlong <developerlin123@gmail.com>
2023-01-19 19:08:05 +0100

    store and fetch recipes

--- src/app/recipes/recipe_services/recipe.service.ts
+++ src/app/recipes/recipe_services/recipe.service.ts
@@ -9,42 +9,49 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeService {
 
-  private recipes : Recipe[] = [
-    new Recipe(
-      "pasta",
-      "very good",
-      "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FEdit%2F09-2022-sausage-pasta%2Fsausage-pasta-4",
-      [
-        new Ingredient('tomato',3),
-        new Ingredient('mushroom',15),
-        new Ingredient('garlic',3)
-      ]),
-    new Recipe(
-      "noodle",
-      "best in the town",
-      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/c4e7f587-e9e1-4953-b032-f58f82e91f4d/Derivates/4b8b814f-8562-4ad0-8db8-9939cb103685.jpg",
-      [
-        new Ingredient('onion',1),
-        new Ingredient('pork',20),
-        new Ingredient('parsley',10)
-      ]
-      ),
-    new Recipe(
-      "paella",
-      "eggplant",
-      "https://eatingoutorin.com/wp-content/uploads/2022/01/Mana-75-10-1024x768.jpg",
-      [ 
-        new Ingredient('green bean',15),
-        new Ingredient('chicken leg',6),
-        new Ingredient('snail',10)
-      ]
-      ),
-  ]
+  // private recipes : Recipe[] = [
+  //   new Recipe(
+  //     "pasta",
+  //     "very good",
+  //     "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FEdit%2F09-2022-sausage-pasta%2Fsausage-pasta-4",
+  //     [
+  //       new Ingredient('tomato',3),
+  //       new Ingredient('mushroom',15),
+  //       new Ingredient('garlic',3)
+  //     ]),
+  //   new Recipe(
+  //     "noodle",
+  //     "best in the town",
+  //     "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/c4e7f587-e9e1-4953-b032-f58f82e91f4d/Derivates/4b8b814f-8562-4ad0-8db8-9939cb103685.jpg",
+  //     [
+  //       new Ingredient('onion',1),
+  //       new Ingredient('pork',20),
+  //       new Ingredient('parsley',10)
+  //     ]
+  //     ),
+  //   new Recipe(
+  //     "paella",
+  //     "eggplant",
+  //     "https://eatingoutorin.com/wp-content/uploads/2022/01/Mana-75-10-1024x768.jpg",
+  //     [ 
+  //       new Ingredient('green bean',15),
+  //       new Ingredient('chicken leg',6),
+  //       new Ingredient('snail',10)
+  //     ]
+  //     ),
+  // ]
+
+  private recipes:Recipe[]=[]
 
   recipesChanged=new Subject<Recipe[]>()
 
   constructor(private slService:ShoppingListService) { }
 
+  setRecipes(recipes:Recipe[]){
+    this.recipes=recipes
+    this.recipesChanged.next(this.recipes.slice())
+  }
+   
   getRecipes(){
     return this.recipes.slice() // return a new arr object
   }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe_services/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private httpClient: HttpClient) { }
-    
+    constructor(private http: HttpClient, private recipeService:RecipeService) { }
+
+    storeRecipes(){
+      const recipes=this.recipeService.getRecipes()
+      this.http.put(
+        'https://brocclivshop-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      ).subscribe((res) => {
+        console.log(res)
+      })
+    }
+
+    fetchRecipes(){
+      this.http
+      .get<Recipe[]>('https://brocclivshop-default-rtdb.firebaseio.com/recipes.json')
+      .subscribe((res) => {
+        console.log(res)
+        this.recipeService.setRecipes(res)
+      })
+    }
 }
