3740406575faf70b07b0847547c48f93768d9f2e
linlong <developerlin123@gmail.com>
2023-01-23 16:13:26 +0100

    merge sign in and sign up,extract error handling logic

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,45 +1,65 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResData{
+export interface AuthResData{
   idToken:string,
   email:string,
   refreshToken:string,
   expiresIn:string,
-  localId:string
+  localId:string,
+  registered?:string
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(private http:HttpClient) { }
-
   signUp(email:string,password:string){
-
     const API_KEY='AIzaSyBmKK5_b36ULLLtXNrZkdkrk723-vJ8dMA'
-
     const signUpUrl='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key='+`${API_KEY}`
-    console.log('signUpUrl:',signUpUrl)
     return this.http.post<AuthResData>(signUpUrl,{
       email:email,
       password:password,
       returnSecureToken:true
     })
-    .pipe(catchError((errRes) => {
-       let errorMessage='An error occurred!!!'
-       if(!errRes.error || !errRes.error.error){
-        return throwError(errorMessage)
-       }
-       errorMessage=errRes.error.error.message
-       return throwError(errorMessage)
-     }
-    ))
+    .pipe(
+      catchError(this.handleError)
+    )
   }
 
-  login(){
+  login(email:string,password:string){
+    const API_KEY='AIzaSyBmKK5_b36ULLLtXNrZkdkrk723-vJ8dMA'
+    const signInUrl='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='+`${API_KEY}`
+    return this.http.post<AuthResData>(signInUrl,{
+      email:email,
+      password:password,
+      returnSecureToken:true
+    })
+    .pipe(
+      catchError(this.handleError)
+    )
+  }
 
+  private handleError(errRes:HttpErrorResponse){
+    let errorMessage='An error occurred!!!'
+    if(!errRes.error || !errRes.error.error){
+     return throwError(errorMessage)
+    }
+
+    switch (errRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage='This email exists already.'
+        break;    
+      case 'EMAIL_NOT_FOUND':
+        errorMessage='This email doesnt exist.'
+        break;    
+      case 'INVALID_PASSWORD':
+        errorMessage='This password is not correct.'
+        break;    
+    }
+
+    return throwError(errorMessage)
   }
 }
--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+import { Observable } from 'rxjs';
+import { AuthService,AuthResData } from '../auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,44 +9,39 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
   isLoginMode=true
   isLoading=false
   error:string=null
 
   constructor(private authService:AuthService) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  
   onSubmit(form:NgForm){
+    let authObs:Observable<AuthResData>;
      if(!form.valid){
       return
      }
      this.isLoading=true
      const email=form.value.email
      const password=form.value.password
+
      if(this.isLoginMode){
-        console.log('this is login')
+      authObs=this.authService.login(email,password)
      }else{
-        this.authService.signUp(email,password)
-        .subscribe(
-          (signUpRes) => {
-            console.log('signUpRes---',signUpRes)
-            this.isLoading=false
-          },(errorMessage) => {
-            this.error=errorMessage
-            this.isLoading=false
-          }
-        )
+      authObs=this.authService.signUp(email,password)
      }
+     authObs.subscribe(
+       (res) => {
+         console.log("res---",res)
+         this.isLoading=false
+       },(errorMessage) => {
+         this.error=errorMessage
+         this.isLoading=false
+       }
+     )
      form.reset()
   }
-
   onSwitchMode(){
     this.isLoginMode=!this.isLoginMode
   }
-
-
-
 }
