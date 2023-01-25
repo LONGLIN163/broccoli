fcc6046b81479da087f144ff38b238761e327b5e
linlong <developerlin123@gmail.com>
2023-01-25 16:22:14 +0100

    outsourcing recipes related logic

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { AuthGuard } from '../auth/auth/auth.guard';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesComponent } from './recipes.component';
+import { RecipesResolverService } from './recipe_services/recipes-resolver.service';
+import { RouterModule } from '@angular/router';
+
+const recipesRoutes=[
+  {
+    path:'recipes',
+    component:RecipesComponent,
+    canActivate:[AuthGuard],
+    children:[
+        {path:'',component:RecipeStartComponent},
+        {path:'new',component:RecipeEditComponent},
+        {
+            path:':id',
+            component:RecipeDetailComponent,
+            resolve:[RecipesResolverService]
+        },
+        {
+            path:':id/edit',
+            component:RecipeEditComponent,
+            resolve:[RecipesResolverService]
+        }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports:[
+    RouterModule.forChild(recipesRoutes)
+  ],
+  exports:[RouterModule]
+})
+export class RecipesRoutingModule { }
